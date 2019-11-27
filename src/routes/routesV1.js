5982a096966dbf1e1b6c286aba66ca4605635707
peretch <sebastian@peretch.com>
2019-11-26 21:07:14 -0300

    Added function to obtain token from email and password

--- src/routes/routesV1.js
+++ src/routes/routesV1.js
@@ -19,13 +19,25 @@ module.exports = (app) => {
     'use strict';
     const router = express.Router();
     
-
+    /**
+     * This function returns general API's information
+     */
     router.get('/', (req, res) => {
         res.json({
-            hola:   'mundo'
+            apiInformation: {
+                version:   '1.0',
+                releaseDate: new Date('2019-11-28').toString()
+            }
         });
     });
 
+    /**
+     * Creates a new user and gives one token to interact with all the APIs.
+     * @param {String} email    - Unique user's email
+     * @param {String} password - Pasword for the user
+     * @param {String} name     - User's name
+     * @returns {User} Created user
+     */
     router.post('/users', json(), (req, res) => {
         const userPayload = req.body;
 
@@ -53,7 +65,7 @@ module.exports = (app) => {
                     res
                     .status(401)
                     .json({
-                        message: `El usuario con el email ${err.keyValue.email} ya existe en el sistema.`
+                        message: `The given email '${err.keyValue.email}' is alredy in use.`
                     })
                 }else{
                     res
@@ -63,6 +75,47 @@ module.exports = (app) => {
                     });
                 }
             })
+    });
+
+    /**
+     * Given an email and his password, returns a token to interact with all the APIs.
+     * @param {String} email    - Unique user's email
+     * @param {String} password - Pasword for the user
+     * @returns {String} Bearer token
+     */
+    router.post('/sessions', json(), (req, res) => {
+        const userPayload = req.body;
+
+        User.findOne({ email: userPayload.email })
+            .then((userDoc) => {
+                return Promise.all([
+                    userDoc,
+                    compare(
+                        userPayload.password, 
+                        userDoc.password
+                    )
+                ]);
+            })
+            .then(([{email}]) =>  {
+                const token = sign({ email }, JWT_SECRET);
+
+                res.json({
+                    user: email,
+                    token
+                })
+            })
+            .catch((error) => {
+                res
+                    .status(400)
+                    .json({
+                        error: error.message
+                    });
+            })
+    })
+
+    router.put('/users', json(), checkJwt({ secret: JWT_SECRET }), (req, res) =>{
+        const userPayload = req.body;
+        res.json(userPayload);
     });
 
     app.use('/v1', router);
